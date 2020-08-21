adb379a653de8c2ae301cb8c39b3678ba194828c
EC2 Default User <ec2-user@ip-172-31-25-108.us-west-2.compute.internal>
2020-08-21 19:59:12 +0000

    changes to remove conf.conf dependency

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ sys.path.append('../')
 #import nodes
 
 from .ProcessingNetwork import ProcessingNetwork
-from .PersistanceNetwork import PersistanceNetwork
+#from .PersistanceNetwork import PersistanceNetwork
 from .ProcessingNode import ProcessingNode 
-from .ProcessingNode import QuickInputNode 
-from .ProcessingNode import QuickProcessingNode 
+#from .ProcessingNode import QuickInputNode 
+#from .ProcessingNode import QuickProcessingNode 
